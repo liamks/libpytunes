02231454bf1b5bfe309fa2d833bf3aee22377753
Clayton Parker <clayton@clyde.local>
2009-10-08 08:24:38 -0400

    Added a setup.py so this can be easy_install'd

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyItunes",
+    version="1.0",
+    author="",
+    author_email="",
+    url="",
+    description="This is a package",
+    long_description=open("README.txt").read(),
+    classifiers=[],
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=[]
+    )
+
