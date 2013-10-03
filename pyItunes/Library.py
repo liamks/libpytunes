b012c91a89ec4f7cd2548f24785331007f65f488
copart <git@rellims.com>
2013-10-03 16:04:58 -0400

    Updated to use actual python xml parser (works better with special characters), location conversion, depreciated XMLLibraryParser library

--- pyItunes/Library.py
+++ pyItunes/Library.py
@@ -1,13 +1,28 @@
 from pyItunes.Song import Song
 import time
-class Library:
-	def __init__(self,dictionary):
-		self.songs = self.parseDictionary(dictionary)
+import plistlib
+import urlparse
+import time
 
-	def parseDictionary(self,dictionary):
-		songs = []
-		format = "%Y-%m-%dT%H:%M:%SZ"
-		for song,attributes in dictionary.iteritems():
+class Library:
+	def __init__(self,itunesxml,keyByTrackID=False,musicPathXML=None,musicPathSystem=None):
+		if type(itunesxml) == str:
+			self.il = plistlib.readPlist(itunesxml) #Much better support of xml special characters
+		else:
+			self.il = {}
+			self.il['Tracks'] = itunesxml
+		self.keyByTrackID = keyByTrackID
+		if keyByTrackID:
+			self.songs = {}
+		else:
+			self.songs = []
+		self.musicPathXML = musicPathXML
+		self.musicPathSystem = musicPathSystem
+		self.getSongs()
+
+	def getSongs(self):
+		format = "%Y-%m-%d %H:%M:%S"
+		for trackid,attributes in self.il['Tracks'].iteritems():
 			s = Song()
 			s.name = attributes.get('Name')
 			s.artist = attributes.get('Artist')
@@ -16,7 +31,6 @@ class Library:
 			s.album = attributes.get('Album')
 			s.genre = attributes.get('Genre')
 			s.kind = attributes.get('Kind')
-			s.grouping = attributes.get('Grouping')
 			if attributes.get('Size'):
 				s.size = int(attributes.get('Size'))
 			s.total_time = attributes.get('Total Time')
@@ -24,9 +38,9 @@ class Library:
 			if attributes.get('Year'):
 				s.year = int(attributes.get('Year'))
 			if attributes.get('Date Modified'):
-				s.date_modified = time.strptime(attributes.get('Date Modified'),format)
+				s.date_modified = time.strptime(str(attributes.get('Date Modified')),format)
 			if attributes.get('Date Added'):
-				s.date_added = time.strptime(attributes.get('Date Added'),format)
+				s.date_added = time.strptime(str(attributes.get('Date Added')),format)
 			if attributes.get('Bit Rate'):
 				s.bit_rate = int(attributes.get('Bit Rate'))
 			if attributes.get('Sample Rate'):
@@ -37,7 +51,18 @@ class Library:
 			if attributes.get('Play Count'):
 				s.play_count = int(attributes.get('Play Count'))
 			if attributes.get('Location'):
-				s.location = attributes.get('Location')			
-			songs.append(s)
+				if ( self.musicPathXML is None or self.musicPathSystem is None ):
+					s.location = urlparse.unquote(urlparse.urlparse(attributes.get('Location')).path[1:])
+				else:
+					s.location = urlparse.unquote(urlparse.urlparse(attributes.get('Location')).path[1:]).replace(self.musicPathXML,self.musicPathSystem)
 			s.compilation = 'Compilation' in attributes
-		return songs
+			if attributes.get('Play Date UTC'):
+				s.lastplayed = time.strptime(str(attributes.get('Play Date UTC')),format)
+			if attributes.get('Total Time'):
+				s.length = int(attributes.get('Total Time'))
+			if attributes.get('Grouping'):
+				s.grouping = attributes.get('Grouping')
+			if self.keyByTrackID:
+				self.songs[trackid] = s
+			else:
+				self.songs.append(s)
