565f8c87efd27cc0d4b1e5cdac79d1003926d022
Andre-Nam <andrenam.berger@gmail.com>
2014-05-21 21:04:59 +0200

    fixed setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email="",
     url="https://github.com/liamks/pyitunes",
     description="Python Itunes Library parser",
-    long_description=open("README.txt").read(),
+    long_description=open("README.md").read(),
     classifiers=[],
     packages=find_packages(exclude=['ez_setup']),
     namespace_packages=[]
