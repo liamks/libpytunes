174f366de9a5c214f33d2ee22cf854542ceabe1a
Matt Behrens <askedrelic@gmail.com>
2014-08-22 10:49:04 -0400

    adding version 1.3, tracking the licensing change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyItunes",
-    version="1.1",
+    version="1.3",
     license='MIT',
     author="Liam Kaufman",
     author_email="",
