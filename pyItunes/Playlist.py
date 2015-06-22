f83c5cd638e1da104b321e2dac416e50ccb64aec
Roger Filmyer <roger.filmyer@gmail.com>
2015-06-21 21:42:51 -0400

    Replaced PlTracks with Songs
    Instead of having a separate PlTrack class for elements of a playlist,
    I made playlists a list of Songs, with an additional number element
    denoting their position in the playlist.
    
    For the record, I want number to be replaced with playlist_order or
    something else so it doesn't break everything. Not sure if that will break
    compatibility though.

--- pyItunes/Playlist.py
+++ pyItunes/Playlist.py
@@ -1,11 +1,3 @@
-class PlTrack:
-	location = None
-	name = None
-	artist = None
-	length = None
-	number = None
-	album = None
-
 class Playlist:
 	def __init__(self,playListName=None):
 		self.name = playListName
--- pyItunes/__init__.py
+++ pyItunes/__init__.py
@@ -1,4 +1,4 @@
 from pyItunes.XMLLibraryParser import XMLLibraryParser
 from pyItunes.Library import Library
 from pyItunes.Song import Song
-from pyItunes.Playlist import Playlist,PlTrack
+from pyItunes.Playlist import Playlist # ,PlTrack
